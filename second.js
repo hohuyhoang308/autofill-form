af8998cd6cc8c928320814c7e25f9720aeb3fbac
hohuyhoang308 <85646489+hohuyhoang308@users.noreply.github.com>
2024-07-20 21:31:13 +0700

    Create second.js

---
+++ second.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name         autofill
+//@author        Hồ Huy Hoàng
+// @namespace    carl.3h
+// @version      1.4
+// @description  tự động đánh giá khảo sát
+// @include      office.com
+// @match        https://forms.office.com/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM.xmlHttpRequest
+// @grant        GM_getResourceText
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        unsafeWindow
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+document.addEventListener("keydown", function(event) {
+  if (event.key === "`") {
+    // Lấy phần tử input
+const inputElement = document.querySelector('input[aria-label="Single line text"]');
+if (inputElement) {
+  inputElement.value = "Hài lòng";
+} else {
+  console.log("Không Tìm Thấy Text Box");
+}
+// Lấy tất cả các phần tử input radio
+const radioInputs = document.querySelectorAll('input[role="radio"]');
+
+//Chọn Mục Muốn Đánh Giá
+const radioAim = "Rất hài lòng";
+      // Lọc các phần tử input radio có chuỗi "includes("")" trong aria-label
+const filteredRadioInputs = Array.from(radioInputs).filter((radioInput) => {
+  const ariaLabel = radioInput.getAttribute('aria-label');
+  return ariaLabel.includes(radioAim);
+});
+
+    // Số lần click
+    let clickCount = 0;
+
+    // Kích hoạt sự kiện click vào radio button mỗi giây
+    const interval = setInterval(() => {
+      // Kiểm tra xem đã click hết tất cả các radio button hay chưa
+      if (clickCount < filteredRadioInputs.length) {
+        // Click vào radio button tiếp theo
+        filteredRadioInputs[clickCount].click();
+          console.log("Đã Click Đánh Giá",radioAim , "ID :" ,clickCount);
+        clickCount++;
+      } else {
+        // Dừng interval nếu đã click hết tất cả các radio button
+        clearInterval(interval);
+
+        // Đợi 2 giây trước khi ấn nút Submit
+        setTimeout(() => {
+          // Tìm phần tử nút Submit
+        //  const submitButton = document.querySelector('button[data-automation-id="submitButton"]');
+
+          // Kích hoạt sự kiện click vào nút Submit
+          submitButton.click();
+        }, 2000000666650000000);
+      }
+    }, 25);
+  }
+});
+})();
